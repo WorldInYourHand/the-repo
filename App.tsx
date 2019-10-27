21f8d39c2fe5caeb63472533d9edfdb9edd511cc
dimitar.hristov <dimitar.hristov@mentormate.com>
2019-10-27 13:02:11 +0200

    added some state changes and a button check them out

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  let text1 = "Бачка яко луд"
+  let text2 = "Почна се!!!"
+  const[outputText, setOutputText] = useState( text1)
   return (
     <View style={styles.container}>
-      <Text>Бачка яко луд</Text>
+      <Text>{outputText}</Text>
+      <Button title="бутончи" onPress={() => setOutputText(text2)} />
     </View>
   );
 }

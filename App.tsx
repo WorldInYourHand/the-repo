06616bf95c92f7ed6010ee18717a522897d8c00e
dimitar.hristov <dimitar.hristov@mentormate.com>
2019-10-27 13:03:36 +0200

    some comments

--- App.tsx
+++ App.tsx
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  {/* let not use hardcoded strings in the code*/}
   let text1 = "Бачка яко луд"
   let text2 = "Почна се!!!"
   const[outputText, setOutputText] = useState( text1)
   return (
     <View style={styles.container}>
       <Text>{outputText}</Text>
+      {/* button with onPress function*/}
       <Button title="бутончи" onPress={() => setOutputText(text2)} />
     </View>
   );

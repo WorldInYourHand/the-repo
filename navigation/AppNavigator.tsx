a7fb4e20c9aefba7437601e3fe426d5671854349
dimitar.hristov <dimitar.hristov@mentormate.com>
2019-11-09 23:22:48 +0200

    cleaning comented code

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
-// import MainTabNavigator from './MainTabNavigator';
-// import LogInOne from '../screens/login-screen-firebase';
-
-// export default createAppContainer(
-//   createSwitchNavigator({
-//     // You could add another route here for authentication.
-//     // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-//     Main: LogInOne,
-//   })
-// );
-
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import LogInOne from "../screens/login-screen-firebase";
